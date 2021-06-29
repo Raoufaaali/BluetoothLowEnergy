a7d851e86d6853dc29341283dcbae852b2b17db3
Raouf <raouf.ali@aristocrat.com>
2021-06-29 13:52:51 -0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx/';
 import { BluetoothLE } from '@ionic-native/bluetooth-le/ngx';
+import { Diagnostic } from '@ionic-native/diagnostic/ngx/';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [ BluetoothLE, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [ BluetoothLE, AndroidPermissions, Diagnostic, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/bl-connect/bl-connect.page.ts
+++ src/app/bl-connect/bl-connect.page.ts
@@ -1,15 +1,92 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx/';
+import { BluetoothLE, InitParams, ScanParams, ScanStatus } from '@ionic-native/bluetooth-le/ngx';
+import { Diagnostic } from '@ionic-native/diagnostic/ngx/';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-bl-connect',
   templateUrl: './bl-connect.page.html',
   styleUrls: ['./bl-connect.page.scss'],
 })
-export class BlConnectPage implements OnInit {
+export class BlConnectPage {
 
-  constructor() { }
 
-  ngOnInit() {
+ public unpairedDevices: any[];
+ public pairedDevices: any;
+ public gettingDevices: boolean;
+ 
+
+  constructor(private ble: BluetoothLE, private alertController: AlertController, private androidPermissions: AndroidPermissions, private diagnostic: Diagnostic) { }
+
+  public ionViewWillEnter()
+  {
+    this.initBluetoothOnDevice();
+    console.log('will Enter');
+  }
+
+  public initBluetoothOnDevice()
+  {
+    let initParms: InitParams = {request: true, restoreKey: 'PlayerMax Lock'};
+    this.ble.initialize(initParms).subscribe(status => console.log(status));1
+  }
+
+  public async startScanning()
+  {
+    this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION).then((data:any) => {
+      if(data.hasPermission) {
+         console.log("have permission ACCESS_COARSE_LOCATION");
+      }      
+   });
+
+   this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION).then((data:any) => {
+    if(data.hasPermission) {
+       console.log("have permission ACCESS_FINE_LOCATION");
+    }      
+ });
+     
+
+    let scanPrams: ScanParams = {};
+    scanPrams.isConnectable = true;
+    // this.ble.startScan(scanPrams).subscribe(scanStatus => console.log(scanStatus.name));
+    this.startScan();
+
+  }
+
+  public startScan() {
+    console.log('Scanning for Bluetooth LE Devices');
+    this.unpairedDevices = [];  // clear list
+    this.ble.startScan({ services: [] }).subscribe((successScanStatus) => {
+     // debugger;
+      console.log("startScan: " + JSON.stringify(successScanStatus));
+      this.addUniqueDevicesToList(successScanStatus);
+    }, (error) => {
+      console.log("error: " + JSON.stringify(error));
+    })
+    setTimeout(() => {
+      console.log('Stopping the scan');
+      this.disconnect()
+
+    }, 5000, 'Scan complete');
+  }
+
+  public async disconnect()
+  {
+    var scanStopedStatus = await this.ble.stopScan();
+    console.log(scanStopedStatus);
+
+  }
+
+  public addUniqueDevicesToList(scanStatus: ScanStatus)
+  {
+    if (this.unpairedDevices.indexOf(scanStatus.address) === -1)
+    {
+      this.unpairedDevices.push(scanStatus);
+    }
+  }
+
+  public selectDevice(deviceAny)
+  {
+
   }
 
 }
